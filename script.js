b462b0cec1b06ffce6828bcfe70827f6aedf28b4
jminervino <joaovictor_minervino@hotmail.com>
2022-02-04 14:40:39 -0300

    Commit HTML & CSS

---
+++ script.js
@@ -0,0 +1,13 @@
+let hour = 0
+let minute = 0
+let second = 0
+let millisecond = 0
+
+function pausar(){
+    let pausa = document.getElementById("pause")
+    return pausa
+}
+
+do{
+    millisecond++
+}while(pausar() == true)
